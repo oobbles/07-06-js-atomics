01c0c15f2256c1b8e2b88e4b18e5eeab97967782
Lisa Davis <oobbles@gmail.com>
2015-07-06 13:17:15 -0500

    Comlete atomic 4

--- input-prompt/global.js
+++ input-prompt/global.js
@@ -1,1 +1,17 @@
-window.onload = function(){
+var sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
+alert(sign)
+
+var name = prompt("Your name?", "Lisa Pisa");
+var age = prompt("Your age?");
+var hometown = prompt("Your hometown?");
+var favoriteNum = prompt("Favorite number?");
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Hometown " + hometown);
+console.log("Your favorite number is " + favoriteNum);
+
+alert("Name " + name);
+alert("Age: " + age);
+alert("Hometown " + hometown);
+alert("Your favorite number is " + favoriteNum);
