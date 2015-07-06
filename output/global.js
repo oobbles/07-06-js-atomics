c7d762a4d61bb60961d31426a9ebb45ef513c4f0
Lisa Davis <oobbles@gmail.com>
2015-07-06 11:50:37 -0500

    Complete atomic 2

---
+++ output/global.js
@@ -0,0 +1,9 @@
+window.alert("greetings green bean");
+
+console.log("I have five cats");
+console.log("I love pizza");
+console.log("I like pasta");
+console.log("Really love most foods");
+console.log("And most music");
+
+window.alert("later tater");
